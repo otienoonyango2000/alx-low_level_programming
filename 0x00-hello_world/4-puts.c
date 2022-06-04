5782719a74655c4c056a0bcacc97ba8ac8b27724
otienoonyango2000 <otienoonyango2000@gmail.com>
2022-06-04 14:50:59 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,6 @@
  *    * *Return:Always 0(success)
  *     **/
 int main(void)
-{     puts("\"programming is like building a
+{     puts("\"Programming is like building a
 		multilingual puzzle)};
 		return(0);
