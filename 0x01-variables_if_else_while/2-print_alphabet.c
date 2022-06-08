e09170a54be884322540d7bf321db4986c1cb94c
otienoonyango2000 <otienoonyango2000@gmail.com>
2022-06-08 11:14:27 -0700

    best work

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+/**
+ *
+ * main -check the code
+ *
+ * return: Always 0
+ */
 
 int main()
 
 {
+	/* print lowercase letter*/
 	int ch;
 	for(ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
