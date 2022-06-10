28e819b8002a42afe9d8b5d9ea5efc48129e7db1
otienoonyango2000 <otienoonyango2000@gmail.com>
2022-06-10 14:36:39 -0700

    alltasks

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+
+int main()
+{
+	if(n < 98)
+	{
+	while(n <= 98)
+	{
+
+	printf("%d", n);
+	if(n != 98)
+	{
+		printf(",");
+	}
+	n++
+
+	}
+
+}
+else if(n > 98)
+{
+
+	while(n >= 98)
+	{
+		printf("%d", n);
+		if(n != 98)
+		{
+			printf(",");
+		}
+		n--;
+	}
+}
+else
+{
+	printf("98");
+}
+	
+	printf("\n");
+
+	
+	return (0);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+}
