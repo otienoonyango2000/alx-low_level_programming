5c97c02239881ab0e605714cbf0cc3ad70c55dc9
otienoonyango2000 <otienoonyango2000@gmail.com>
2022-06-14 07:05:45 -0700

    task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+int _isupper(int c)
+{
+	if(c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else if (c <= 'Z')
+	{
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
+
+
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+This folder contains more nested loops
